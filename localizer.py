f1bd0b959ef629ba360c8e7f681e713eaf1d1ebd
jin-sunghoon <jinsunghoon0825@naver.com>
2025-04-03 18:27:38 +0900

    Finish

--- localizer.py
+++ localizer.py
@@ -17,9 +17,20 @@ def initialize_beliefs(grid):
 def sense(color, grid, beliefs, p_hit, p_miss):
     new_beliefs = []
 
-    #
-    # TODO - implement this in part 2
-    #
+    for i, row in enumerate(beliefs):
+        new_row = []
+        for j, cell in enumerate(row):
+            if grid[i][j] == color:
+                new_beliefs = cell * p_hit
+            else:
+                new_beliefs = cell * p_miss
+            new_row.append(new_beliefs)
+        new_beliefs.append(new_row)
+
+    total_belief = sum(sum(row) for row in new_beliefs)
+    for i in range(len(new_beliefs)):
+        for j in range(len(new_beliefs[i])):
+            new_beliefs[i][j] /= total_belief        
 
     return new_beliefs
 
